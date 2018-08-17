ca1cdf5b74a33d52b4a95ffb99acb5f6218573f5
josuesf <113552@unsaac.edu.pe>
2018-08-16 19:45:15 -0500

    estado escribiendo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var bodyParser = require('body-parser')
 var arrayConectados = []
 
 const URL_DJANGO = 'http://omaralex.pythonanywhere.com'//'http://127.0.0.1:8000'
@@ -12,6 +13,11 @@ var server = app.listen(port, function (err) {
 	if (err) return console.log('Hubo un error'), process.exit(1);
 	console.log('Escuchando en el puerto 3000');
 })
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 var io = require('socket.io')(server);
 function IdSocket(id_usuario) {
 	//console.log(arrayConectados)
@@ -21,34 +27,24 @@ function IdSocket(id_usuario) {
 	return u ? u.socketId : undefined
 }
 app.post('/ws/get_estado_usuario/', function (req, res) {
-  const campo = "saved." + req.body.id_usuario
-  if(IdSocket(req.body.id_usuario)){
-	res.json({respuesta:'ok',estado:'en linea'})
-  }else{
-	request.post(
-		URL_DJANGO + '/ws/get_last_connected',
-		{
-			json: {
-				usuario: req.body.id_usuario
+	const campo = "saved." + req.body.id_usuario
+	if (IdSocket(req.body.id_usuario)) {
+		res.json({ respuesta: 'ok', estado: 'en linea' })
+	} else {
+		request.post(
+			URL_DJANGO + '/ws/get_last_connected',
+			{
+				json: {
+					usuario: req.body.id_usuario
+				}
+			},
+			(error, response, body) => {
+				console.log("ultima vez")
+				console.log(body.data)
+				res.json({ respuesta: 'ok', estado: body.data })
 			}
-		},
-		function (error, response, body) {
-			console.log("ultima vez")
-			console.log(body.data)
-			res.json({respuesta:'ok',estado:body.data})
-		}
-	);
-  }
-  console.log(campo)
-  var posts = db.collection('posts')
-    .find({ [campo]: true })
-    .skip(10 * (req.body.page - 1)).limit(10)
-    .sort({ createdAt: -1 })
-    .toArray((err, posts) => {
-      // If there aren't any posts, then return.
-      if (err) return res.json({ res: "error", detail: err });
-      res.json({ res: "ok", posts });
-    });
+		);
+	}
 })
 io.sockets.on('connection', function (socket) {
 	//console.log(io.sockets.connected[socket.id].emit("prueba",{}))
@@ -130,7 +126,7 @@ io.sockets.on('connection', function (socket) {
 			{
 				json: {
 					id_mensaje: data.id_mensaje,
-					estado_mensaje: (data.estado_mensaje=='visto')?'visto_fin':data.estado_mensaje,
+					estado_mensaje: (data.estado_mensaje == 'visto') ? 'visto_fin' : data.estado_mensaje,
 				}
 			},
 			function (error, response, body) {
@@ -138,14 +134,14 @@ io.sockets.on('connection', function (socket) {
 				console.log(body)
 				var dataResponse = {}
 				dataResponse['id_mensaje'] = body.data.id_mensaje
-				dataResponse['estado_mensaje'] = (data.estado_mensaje=='visto')?'visto_fin':data.estado_mensaje
+				dataResponse['estado_mensaje'] = (data.estado_mensaje == 'visto') ? 'visto_fin' : data.estado_mensaje
 
 				//----- confirmacion al emisor
 				let id_socket_e = IdSocket(body.data.id_e)
 				if (id_socket_e)
 					io.sockets.connected[id_socket_e].emit('status_message', dataResponse)
 				//----- envio mensaje receptor
-				io.sockets.connected[socket.id].emit('status_message',dataResponse)
+				io.sockets.connected[socket.id].emit('status_message', dataResponse)
 
 
 			}
@@ -195,6 +191,14 @@ io.sockets.on('connection', function (socket) {
 		});
 		
 		res.send(returndata);*/
+	});
+	socket.on('escribiendo', function (data) {
+		console.log('alguien escribe a '+data.id_r)
+		let id_socket_r = IdSocket(data.id_r)
+		//console.log(id_socket_r)
+		if (id_socket_r)
+			io.sockets.connected[id_socket_r].emit('escribiendo', data)
+
 	});
 });
 
